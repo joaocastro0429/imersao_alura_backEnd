17c7dde74a78c47beb6e2baedb0a49bbd0e73085
joaocastro <joaocastro0429@gmail.com>
2024-11-19 05:54:29 -0300

    criando banck end em node

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app= express()
+
+app.listen(3333,()=>{
+    console.log("Servidor escutando...");
+})
+
+app.get("/api",(req,res)=>{
+    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+})
